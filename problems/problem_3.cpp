1f625530a90d1173907a7bbafe1a651b65a73541
Laylo123 <150154168+Laylo123@users.noreply.github.com>
2023-11-14 23:07:05 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,7 +2,25 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
-
+   switch (S) {
+        case 'M':
+            if (height < 1.70) {
+                result = "Short";
+            } else if (height>=1.70&&height < 1.85) {
+                result = "Normal";
+            } else {
+                result = "Tall";
+            }
+            break;
+        case 'F':
+            if (height<1.60) {
+                result = "Short";
+            }else if(height>1.60&&height <1.75) {
+                  result = "Normal";
+            }else {
+                result = "Tall";
+            }
+            break;
     // use return to return your result
     // make use of control flow statements
 }

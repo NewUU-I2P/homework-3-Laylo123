c8d4064c712985c16b9cc491068162848c5b2496
Laylo123 <150154168+Laylo123@users.noreply.github.com>
2023-11-14 14:22:31 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,20 @@
 float problemSolution2(float i, float j, float k) {
     float result;
-    // write your code here;
+    float i,j,k;
+     float result;
+     cin >> i >> j >> k;
+     if (i>j && i>k){
+         cout << "The maximum number is  " << i;
+
+     }else {
+         if (j>i && j>k){
+             cout << "The maximum number is  " << j;
+         }else{
+             if (k>j && k>i){
+                 cout << "The maximum number is  " << k;
+             }
+         }
+     }
 
     return result;
 }

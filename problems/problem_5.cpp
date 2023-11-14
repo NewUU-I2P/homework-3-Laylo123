39dfd6c90038cbcd9d9328b435a0e8c820cb52e0
Laylo123 <150154168+Laylo123@users.noreply.github.com>
2023-11-14 23:19:36 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,7 +1,6 @@
 float problemSolution5(float x, float y, char operation) {
-   float result;
-   // write your code here
-switch (operatiion){
+   float result;  
+switch (operatiion) {
    case '+':
       result = x+y;
       break;
